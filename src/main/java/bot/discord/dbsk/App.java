901a50ba6f8585b4839cf75c95f3957cb4d33438
Amoop <44041028+Amoop@users.noreply.github.com>
2018-12-12 15:28:17 -0800

    Add files via upload

--- src/main/java/bot/discord/dbsk/App.java
+++ src/main/java/bot/discord/dbsk/App.java
@@ -116,7 +116,7 @@ public class App
         else if (command.equals("flip")){
             int flip=(int)(Math.random()*2)+1;
             if (flip==1){
-                discord.sendMessage("Heads :andy:");
+                discord.sendMessage("Heads <:andy:522554768787505154>");
             }
             else if (flip==2){
                 discord.sendMessage("Tails");
